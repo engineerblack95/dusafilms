1612b25a37cfe3c3ee92f3715218c775c0fd50c2
engineerblack95 <engineerblack95@gmail.com>
2025-12-14 22:28:29 -0800

    Add auto superuser creation for Render

---
+++ accounts/management/commands/createsu.py
@@ -0,0 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create superuser automatically on Render"
+
+    def handle(self, *args, **kwargs):
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Superuser env vars not set")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser already exists")
+        else:
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write("Superuser created successfully")
